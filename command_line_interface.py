78364b2fdb77efbd9a33cd385867f09f5355a57e
Peter Chay <pete@Peters-MacBook-Pro.local>
2023-03-18 11:11:32 -0700

    Finished super basic logic for pig latin. Currently can only translate one word. Need to revise the script to be able to translate a whole sentence.

--- command_line_interface.py
+++ command_line_interface.py
@@ -1,16 +1,28 @@
-# creating a list of consonants from the consonants text file
-with open("consonants.txt", "r") as file:
-    consonants = file.readlines()
-consonants = [item.strip() for item in consonants]
-
-user_input = input("What would you like to translate? ").upper()
+vowels = ["A", "E", "I", "O", "U"]
 suffix = "ay"
 
-# creating an if loop that checks if the first letter of the user input is a consonant
-if user_input[0] in consonants:
+user_input = input("What would you like to translate? ")
+
+# creating an if loop that checks if the first letter and second of the user input is a consonant
+if user_input[0].upper() not in vowels and user_input[1].upper() in vowels:
     user_input = list(user_input)
     first_letter = user_input[0]
     user_input.pop(0)
     user_input.append(first_letter)
     translated_word = "".join(user_input)
-    print(f"{translated_word}ay")
+    print(f"{translated_word.title()}{suffix}")
+
+# Checks to see if the first letter is a consonant and the second letter is a vowel
+elif user_input[0].upper() not in vowels and user_input[1].upper() not in vowels:
+    user_input = list(user_input)
+    first_letter = user_input[0]
+    second_letter = user_input[1]
+    user_input.pop(0)
+    user_input.pop(0)
+    user_input.extend([first_letter, second_letter])
+    translated_word = "".join(user_input)
+    print(f"{translated_word.title()}{suffix}")
+
+# Checks to see if the first letter is a vowel
+elif user_input[0].upper() in vowels:
+    print(f"{user_input.title()}w{suffix}")
