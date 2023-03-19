bf894a5cf035587224a6a5eb8a57f2cb4854dd75
Peter Chay <pete@Peters-MacBook-Pro.local>
2023-03-19 18:56:30 +0900

    Moved some of the logic into functions

--- command_line_interface.py
+++ command_line_interface.py
@@ -1,27 +1,36 @@
 vowels = ["A", "E", "I", "O", "U"]
 suffix = "ay"
 
+
+def first_consonant_second_vowel(user_input_local):
+    english_word = list(user_input_local)
+    first_letter = english_word[0]
+    english_word.pop(0)
+    english_word.append(first_letter)
+    translated_word = "".join(english_word)
+    print(f"{translated_word.title()}{suffix}")
+
+
+def double_consonants(user_input_local):
+    english_word = list(user_input_local)
+    first_letter = english_word[0]
+    second_letter = english_word[1]
+    english_word.pop(0)
+    english_word.pop(0)
+    english_word.extend([first_letter, second_letter])
+    translated_word = "".join(english_word)
+    print(f"{translated_word.title()}{suffix}")
+
+
 user_input = input("What would you like to translate? ")
 
 # creating an if loop that checks if the first letter and second of the user input is a consonant
 if user_input[0].upper() not in vowels and user_input[1].upper() in vowels:
-    user_input = list(user_input)
-    first_letter = user_input[0]
-    user_input.pop(0)
-    user_input.append(first_letter)
-    translated_word = "".join(user_input)
-    print(f"{translated_word.title()}{suffix}")
+    first_consonant_second_vowel(user_input)
 
 # Checks to see if the first letter is a consonant and the second letter is a vowel
 elif user_input[0].upper() not in vowels and user_input[1].upper() not in vowels:
-    user_input = list(user_input)
-    first_letter = user_input[0]
-    second_letter = user_input[1]
-    user_input.pop(0)
-    user_input.pop(0)
-    user_input.extend([first_letter, second_letter])
-    translated_word = "".join(user_input)
-    print(f"{translated_word.title()}{suffix}")
+    double_consonants(user_input)
 
 # Checks to see if the first letter is a vowel
 elif user_input[0].upper() in vowels:
