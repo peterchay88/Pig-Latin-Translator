a0d0e01fbfac06eabc1a54025d3cd1a4e9f591dd
Peter Chay <peterchay88@gmail.com>
2024-07-30 22:15:37 -0600

    Adding in pig latin logic in webapp.py but dealing with a string index out of range error on line 34 in pig_latin.py

--- pig_latin.py
+++ pig_latin.py
@@ -28,7 +28,7 @@ class PigLatin:
         translated_word = "".join(english_word)
         return f"{translated_word}{self.suffix}"
 
-    def translate_word(self):
+    def translate_sentence(self):
         translated_sentence = []
         for words in self.sentence:
             if words[0].upper() not in self.vowels and words[1].upper() in self.vowels:
@@ -41,15 +41,9 @@ class PigLatin:
             # Checks to see if the first letter is a vowel
             elif words[0].upper() in self.vowels:
                 translated_sentence.append(f"{words}w{self.suffix}")
+            translated_sentence[0] = translated_sentence[0].title()
+        pig_latin_sentence = " ".join(translated_sentence)
+        return pig_latin_sentence
 
-    # if words[0].upper() not in vowels and words[1].upper() in vowels:
-    #     translated_word = PigLatin().first_consonant_second_vowel(words)
-    #     translated_sentence.append(translated_word)
-    # # Checks to see if the first letter is a consonant and the second letter is a vowel
-    # elif words[0].upper() not in vowels and words[1].upper() not in vowels:
-    #     translated_word = PigLatin().double_consonants(words)
-    #     translated_sentence.append(translated_word)
-    # # Checks to see if the first letter is a vowel
-    # elif words[0].upper() in vowels:
-    #     translated_sentence.append(f"{words}w{suffix}")
+
 
