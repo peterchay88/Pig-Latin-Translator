517fa75ed5ebb315cb903e740c30e367480e43c5
Peter Chay <peterchay88@gmail.com>
2024-08-03 16:33:30 -0600

    changed suffix and vowels to protected variables

--- pig_latin.py
+++ pig_latin.py
@@ -1,8 +1,8 @@
 class PigLatin:
+    __suffix = "ay"
+    __vowels = ["A", "E", "I", "O", "U"]
 
     def __init__(self, sentence):
-        self.suffix = "ay"
-        self.vowels = ["A", "E", "I", "O", "U"]
         self.sentence = sentence
 
     def first_consonant_second_vowel(self, word):
@@ -15,7 +15,7 @@ class PigLatin:
             translated_word = "".join(letter)
             translated_words.append(translated_word)
         pig_latin_sentence = " ".join(translated_words)
-        return f"{pig_latin_sentence}{self.suffix}"
+        return f"{pig_latin_sentence}{self.__suffix}"
 
     def double_consonants(self):
         english_word = list(self.sentence)
@@ -25,21 +25,21 @@ class PigLatin:
         english_word.pop(0)
         english_word.extend([first_letter, second_letter])
         translated_word = "".join(english_word)
-        return f"{translated_word}{self.suffix}"
+        return f"{translated_word}{self.__suffix}"
 
     def translate_sentence(self):
         translated_sentence = []
         for words in self.sentence.split():
-            if words[0].upper() not in self.vowels and words[1].upper() in self.vowels:
+            if words[0].upper() not in self.__vowels and words[1].upper() in self.__vowels:
                 translated_word = self.first_consonant_second_vowel(words)
                 translated_sentence.append(translated_word)
             # Checks to see if the first letter is a consonant and the second letter is a vowel
-            elif words[0].upper() not in self.vowels and words[1].upper() not in self.vowels:
+            elif words[0].upper() not in self.__vowels and words[1].upper() not in self.__vowels:
                 translated_word = self.double_consonants()
                 translated_sentence.append(translated_word)
             # Checks to see if the first letter is a vowel
-            elif words[0].upper() in self.vowels:
-                translated_sentence.append(f"{words}w{self.suffix}")
+            elif words[0].upper() in self.__vowels:
+                translated_sentence.append(f"{words}w{self.__suffix}")
             translated_sentence[0] = translated_sentence[0].title()
         pig_latin_sentence = " ".join(translated_sentence)
         return pig_latin_sentence
