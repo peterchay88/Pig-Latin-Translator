70709bc261ad92264733a06cb79992b1ae78dfff
Peter Chay <peterchay88@gmail.com>
2024-08-03 16:42:32 -0600

    Introducing pytest for unit testing

---
+++ unit_tests/test_pig_latin_logic.py
@@ -0,0 +1,26 @@
+import pytest
+
+
+class TestPigLatin:
+
+    def test_all_string(self):
+        """
+        This test confirms when we send sentence of only strings through the pig latin translator code
+        we get the expected return.
+        :return:
+        """
+        pass
+
+    def test_sentence_with_numbers(self):
+        """
+        This test confirms when we send a sentence of with strings and numbers it is handled as expected.
+        :return:
+        """
+        pass
+
+    def test_sentence_with_punctuations(self):
+        """
+        This test confirms when we send a sentence of with strings and punctuations it is handled as expected.
+        :return:
+        """
+        pass
