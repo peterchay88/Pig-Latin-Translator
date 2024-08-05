54823dfde11185b9d3cd749948569f127bbe70cc
Peter Chay <peterchay88@gmail.com>
2024-08-04 21:36:46 -0600

    Changes to command_line_interface.py

--- command_line_interface.py
+++ command_line_interface.py
@@ -1,7 +1,10 @@
 from pig_latin.pig_latin import PigLatin
 
-user_input = input("What would you like to translate? ")
-pig_latin = PigLatin(user_input)
-pig_latin_word = pig_latin.translate_sentence()
+print("----------------------------------------")
+print("Welcome to the Pig Latin Translator!")
+print("---------------------------------------- \n \n")
 
-print(pig_latin_word)
+user_input = input("What would you like to translate?\n")
+pig_latin = PigLatin(user_input)
+print("\nTranslation:")
+print(pig_latin.translate_sentence())
